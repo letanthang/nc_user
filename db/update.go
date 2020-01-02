a57409b08c992a3d6f105438a61c7ae902af3ec0
Le Tan Thang <letanthang@gmail.com>
2020-01-02 16:01:56 +0700

    UpdateUser refactoring

--- db/update.go
+++ db/update.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -19,23 +20,31 @@ func AddUser(user *model.User) (interface{}, error) {
 }
 
 func UpdateUser(req *model.UserUpdateReq) (interface{}, error) {
-	user, _ := FindUserByID(req.ID)
 
-	if req.FirstName != "" {
-		user.FirstName = req.FirstName
-	}
-	if req.LastName != "" {
-		user.LastName = req.LastName
-	}
-	if req.Password != "" {
-		user.Password = utils.MD5(req.Password)
+	// user, _ := FindUserByID(req.ID)
+
+	// if req.FirstName != "" {
+	// 	user.FirstName = req.FirstName
+	// }
+	// if req.LastName != "" {
+	// 	user.LastName = req.LastName
+	// }
+	// if req.Password != "" {
+	// 	user.Password = utils.MD5(req.Password)
+	// }
+	var user map[string]interface{}
+
+	bs, _ := json.Marshal(req)
+	json.Unmarshal(bs, &user)
+	if _, ok := user["password"]; ok {
+		user["password"] = utils.MD5(user["password"].(string))
 	}
 
 	fmt.Printf("user :%+v", user)
 
 	collection := Client.Database(DbName).Collection(ColName)
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	filter := bson.M{"id": user.ID}
+	filter := bson.M{"id": req.ID}
 	update := bson.M{"$set": user}
 	res, err := collection.UpdateOne(ctx, filter, update)
 	return res, err
